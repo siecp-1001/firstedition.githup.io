9d582acc897cb2c8ba30807b128379b7ae32e698
siecp-1001 <108583424+siecp-1001@users.noreply.github.com>
2022-12-01 23:21:34 +0300

    newupdate

---
+++ login/stream/stream.js
@@ -0,0 +1,34 @@
+'use strict';
+const video=document.getElementById('video');
+const canvas=document.getElementById('canvas');
+const snap=document.getElementById('snap');
+const errormsgElemt=document.getElementById('spanErrormsg');
+
+const constraints={
+    audio:true,
+    video:{
+        width:1280,
+        height:720}
+};
+
+
+async function init(){
+    try{
+        const stream=await navigator.mediaDevices.getUserMedia(constraints);
+        handleSuccess(stream);
+    }
+    catch(e){
+        errormsgElemt.innerHTML='navigator.getUserMedia.error:${e.toString()}';
+    }
+}
+
+function handleSuccess(stream){
+    window.stream=stream;
+    video.srcObject=stream;
+}
+
+init();
+var context=canvas.getcontext('2d');
+snap.addEventListener('click',function(){
+    context.drawImage(video,0,0,640,480);
+});
